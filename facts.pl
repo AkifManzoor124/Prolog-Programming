001fcef69b5e6bdfe60ff8a49eebb7204d3e7d38
Akif Manzoor <akifmanzoor124@gmail.com>
2020-07-05 12:53:04 -0400

    Created hello world example Created examples for facts definition

---
+++ facts.pl
@@ -0,0 +1,10 @@
+%Definition of facts
+like(dogs,cats).        %dogs like cats
+domestic(cows).         %cows are domenstic
+faster(horses,cows).    %horses are faster than cows
+take(cats, milk, cows). %Cats take milk from cows
+isYellow(hay).          %hay is yellow
+eat(cows,hay).          %cows eat hay
+dog(husky).
+dog(goldenretriever).
+cat(maincoon).
